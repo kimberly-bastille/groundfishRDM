3ec3eb5f2abf2a8ee7dedd21a58e69058de9a2db
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-07-29 10:41:44 -0400

    take out unneeded packages. Add note about main mriptacklebox version. Build list of target trips.

--- Code/pre_sim/get_mrip_trips.R
+++ Code/pre_sim/get_mrip_trips.R
@@ -5,22 +5,21 @@
 #               before switching to other harder metrics (catch by weight)
 # Inputs:       mrip_pull{}.Rds
 # Outputs:      None yet
-# Dependencies: Packages here, mriptacklebox, ROracle, tidyverse, DBI, glue,
-#               haven, conflicted. Sources developer_setup.R (for gf.data.dir).
-#               Requires Oracle access.
+# Dependencies: mriptacklebox
+#               Sources developer_setup.R (for gf.data.dir).
 # Pipeline:     Not in pipeline yet
+#
+# To Do:        Subset geographically.  Disaggregate to "combined mode"
 ################################################################################
 
 
 # Load libraries
-# install the main branch
-#remotes::install_github("NEFSC/READ-PDB-mriptacklebox")
+# install the main branch, needs main on/after  7/29/2026
+# remotes::install_github("NEFSC/READ-PDB-mriptacklebox")
 
 library("here")
 library("mriptacklebox")
-library("ROracle")
 library("tidyverse")
-library("DBI")
 library("glue")
 library("haven")
 library("conflicted")
@@ -47,49 +46,83 @@ mrip_pull<-read_rds(file=file.path(output_folder, glue("mrip_pull{data_vintage}.
 # cast to upper case
 mrip_pull <- map(mrip_pull, ~rename_with(.x, toupper))
 
+# Do this, and then you can use MODE_COMBINED as one of the domains
+# mrip_pull <- mrip_pull %>%
+#   modify_at(c("trip", "catch","size","size_b2"), ~ .x %>%
+#               mutate(MODE_COMBINED = case_when(
+#                 MODE_FX == "SOMETHING" ~ "FORHIRE",
+#                 MODE_FX == "SOMETHING_ELSE" ~ "PRIVATE",
+#                 MODE_FX == "THIRD_THING" ~ "SHORE",
+#                 TRUE ~ MODE_FX
+#               ))
+#   )
+
+# Sample code to create a my_dom_id var based on state
 # mrip_pull <- mrip_pull %>%
 #   modify_at("trip", ~ .x %>%
 #               mutate(my_dom_id = case_when(
-#                 PRIM1_COMMON == "ATLANTIC COD" ~ "InDom",
+#                 ST_ABB %in% c("ME", "NH", "MA") ~ "InDom",
 #                 TRUE ~ "NotInDom"
-#               )) %>%
-#               mutate(my_dom_id=glue("{my_dom_id}_{YEAR}{WAVE}"))
+#               )))
 #   )
 
 
 
 
-# mrip_pull <- mrip_pull %>%
-#   modify_at(c("catch","size","size_b2"), ~ .x %>%
-#               mutate(my_dom_id = case_when(
-#                 COMMON == "ATLANTIC COD" ~ "ATLANTIC_COD",
-#                 COMMON == "HADDOCK" ~ "HADDOCK",
-#                 TRUE ~ "NONE"
-#               )) %>%
-#               mutate(my_dom_id=glue("{my_dom_id}_{YEAR}{WAVE}"))
-#   )
+
+types<-list(
+  'PRIM1',
+  'PRIM2',
+  'A',
+  'B1',
+  'B2',
+  c('PRIM1', 'PRIM2', 'A','B1', 'B2'),
+  c('PRIM1', 'PRIM2'),
+  c('A','B1', 'B2'),
+  c('A','B1')
+)
+list_names <- types %>%
+  map_chr(~ paste(.x, collapse = "|"))
 
 
-# Target or caught COD
-targets_COD<-mrip_effort(dom=c("YEAR", "WAVE"),
-                            microdata=mrip_pull,
-                            dir_trip = list(comname = c('ATLANTIC COD'),
-                                            typ = c('PRIM1', 'PRIM2', 'B1', 'B2')))
-# Target or caught Haddock
 
-targets_HADDOCK<-mrip_effort(dom=c("YEAR", "WAVE"),
-                         microdata=mrip_pull,
-                         dir_trip = list(comname = c('HADDOCK'),
-                                         typ = c('PRIM1', 'PRIM2', 'B1', 'B2')))
+targets_COD <- types %>%
+  map(~ mrip_effort(
+    dom = c("YEAR"),
+    microdata = mrip_pull,
+    dir_trip = list(
+      comname = c('ATLANTIC COD'),
+      typ = .x
+    )
+  )) %>%
+  set_names(list_names)
+
+targets_HADDOCK <- types %>%
+  map(~ mrip_effort(
+    dom = c("YEAR"),
+    microdata = mrip_pull,
+    dir_trip = list(
+      comname = c('HADDOCK'),
+      typ = .x
+    )
+  )) %>%
+  set_names(list_names)
 
 # Target or caught Either
 
-targets_EITHER<-mrip_effort(dom=c("YEAR", "WAVE"),
-                            microdata=mrip_pull,
-                            dir_trip = list(comname = c('ATLANTIC COD', 'HADDOCK'),
-                                             typ = c('PRIM1', 'PRIM2', 'B1', 'B2')))
+targets_EITHER <- types %>%
+  map(~ mrip_effort(
+    dom = c("YEAR"),
+    microdata = mrip_pull,
+    dir_trip = list(
+      comname =  c('ATLANTIC COD', 'HADDOCK'),
+      typ = .x
+    )
+  )) %>%
+  set_names(list_names)
 
+# The relationship of some of the entries
+# PRIM1 : Cod + haddock=Either by definition
+# PRIM2: Same
+# Everything else: Either>= Cod, Either>=Haddock, Either<=Cod+Haddock
 
-targets_COD[1,]
-targets_HADDOCK[1,]
-targets_EITHER[1,]
